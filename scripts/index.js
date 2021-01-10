3c754b3062589a205992eea396b5c84565f08af7
Daniil Larionov <red7172@yandex.ru>
2021-01-10 15:26:21 +0400

    fix: исправить недочеты после ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,35 @@
-let editButton = document.querySelector('.profile__edit-button')
-let popup = document.querySelector('.popup')
-let closeButton = popup.querySelector('.popup__close')
-
+let editButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close');
+let form = popup.querySelector('.popup__form');
+let formElement = popup.querySelector('.popup__form');
+let nameInput = popup.querySelector('.popup__input_username');
+let jobInput = popup.querySelector('.popup__input_job');
+let username = document.querySelector('.profile__username');
+let job = document.querySelector('.profile__job');
 let togglePopup = () => {
-  popup.classList.toggle('popup_opened')
-}
-
-editButton.addEventListener('click', togglePopup)
-closeButton.addEventListener('click', togglePopup)
-  
-popup.addEventListener('click', (event) => {
-  if (event.target === event.currentTarget) {
-    togglePopup()
+    popup.classList.toggle('popup_opened');
   }
-})  
 
-let form = popup.querySelector('.popup__form')
-
-form.addEventListener('submit', () => {
-  
-})
-
-let formElement = popup.querySelector('.popup__form')
+function changeInputValue () {
+  nameInput.value = username.textContent;
+  jobInput.value = job.textContent;
+}
 
 function handleFormSubmit (evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                        // Так мы можем определить свою логику отправки.
-                        // О том, как это делать, расскажем позже.
-
-    // Находим поля формы в DOM
-  let nameInput = popup.querySelector('.popup__input_username')
-  let jobInput = popup.querySelector('.popup__input_job')
-
-    // Получите значение полей из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-  let username = document.querySelector('.profile__username')
-  let job = document.querySelector('.profile__job')
-
-    // Вставьте новые значения с помощью textContent
-  username.textContent = nameInput.value
-  job.textContent = jobInput.value
-
-  togglePopup()
+  evt.preventDefault(); 
+  username.textContent = nameInput.value;
+  job.textContent = jobInput.value;
+  togglePopup();
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
+
 formElement.addEventListener('submit', handleFormSubmit);
+editButton.addEventListener('click', changeInputValue);
+editButton.addEventListener('click', togglePopup);
+closeButton.addEventListener('click', togglePopup)  ;
+popup.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget) {
+    togglePopup();
+  }
+})  
 
